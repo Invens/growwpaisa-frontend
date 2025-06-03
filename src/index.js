96f3f730caeb25a4f862c7a3fd4066bf5dc074b3
razzbalram <104671474+razzbalram@users.noreply.github.com>
2025-06-03 11:54:45 +0000

    add meta code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// import { AuthProvider } from './contexts/AuthContext';
+// import './index.css'; // Import your Tailwind CSS file
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <AuthProvider>
+//     <App />
+//     </AuthProvider>
+  
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -5,18 +29,19 @@ import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './contexts/AuthContext';
 import './index.css'; // Import your Tailwind CSS file
 
+// ✅ Inject meta tag into <head>
+const meta = document.createElement('meta');
+meta.name = 'impact-site-verification';
+meta.content = '410337263';
+document.head.appendChild(meta);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthProvider>
-    <App />
+      <App />
     </AuthProvider>
-  
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
